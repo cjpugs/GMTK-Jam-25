f76524681c285c2d3b57ccc214bf9cd7dff7c86b
Jeremy King <codazendev@gmail.com>
2025-07-31 17:17:18 -0400

    finally figured out how to make custom collectible models exhibit their textures too

--- scenes/collectibles/collectible.gd
+++ scenes/collectibles/collectible.gd
@@ -2,10 +2,21 @@ extends Area3D
 
 @export var point_value := 100 # placeholder default score
 @export var mesh_override: Mesh
+@export_enum("Laptop") var collectible_type: String
 
 func _ready() -> void:
 	if mesh_override:
 		$MeshInstance3D.mesh = mesh_override
+	
+	var scene_path := ""
+	match collectible_type:
+		"Laptop": scene_path = "res://scenes/collectibles/laptop.tscn"
+	
+	if scene_path != "":
+		var new_model = load(scene_path).instantiate()
+		$Model.queue_free()
+		add_child(new_model)
+		new_model.name = "Model"
 
 func _on_body_entered(body: Node3D) -> void:
 	Globals.score += point_value
