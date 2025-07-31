62af78541a0e41cb88759dd842a49230105e954e
cjpugs <cjcosta04@gmail.com>
2025-07-31 14:57:46 -0400

    Revert "gravity is toggleable"
    This reverts commit 6a064a2679a2a92b119836e1d65b5bc679d6bd8c.

--- scenes/obstacles/customer.gd
+++ scenes/obstacles/customer.gd
@@ -1,13 +1,4 @@
 extends CharacterBody3D
 
-@export var gravity_enabled : bool = false
-@export var gravity = 10
-
-func _physics_process(delta: float) -> void:
-	if gravity_enabled and not is_on_floor():
-		velocity.y -= gravity * delta
-		move_and_slide()
-
-
-func enable_gravity():
-	gravity_enabled = true
+func enable_grav():
+	print("gravity enabled")
