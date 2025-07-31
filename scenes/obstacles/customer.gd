6a064a2679a2a92b119836e1d65b5bc679d6bd8c
cjpugs <cjcosta04@gmail.com>
2025-07-31 14:57:26 -0400

    gravity is toggleable

--- scenes/obstacles/customer.gd
+++ scenes/obstacles/customer.gd
@@ -1,4 +1,13 @@
 extends CharacterBody3D
 
-func enable_grav():
-	print("gravity enabled")
+@export var gravity_enabled : bool = false
+@export var gravity = 10
+
+func _physics_process(delta: float) -> void:
+	if gravity_enabled and not is_on_floor():
+		velocity.y -= gravity * delta
+		move_and_slide()
+
+
+func enable_gravity():
+	gravity_enabled = true
