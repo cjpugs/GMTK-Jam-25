c1a5171570b02559a975ce6f7fb340d68e65a8ba
Jeremy King <codazendev@gmail.com>
2025-07-31 16:42:32 -0400

    finally added the capability for the mesh of a collectible to be changes in the property editor; added laptop model

--- scenes/collectibles/collectible.gd
+++ scenes/collectibles/collectible.gd
@@ -1,6 +1,11 @@
 extends Area3D
 
 @export var point_value := 100 # placeholder default score
+@export var mesh_override: Mesh
+
+func _ready() -> void:
+	if mesh_override:
+		$MeshInstance3D.mesh = mesh_override
 
 func _on_body_entered(body: Node3D) -> void:
 	Globals.score += point_value
